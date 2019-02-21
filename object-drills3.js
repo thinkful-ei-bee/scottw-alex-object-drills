2d56637d18635f3559ded13ac5d23def3c8a1426
Scott Williams <scottcolemanwilliams@gmail.com>
2019-02-21 11:30:24 -0600

    added meals

--- object-drills3.js
+++ object-drills3.js
@@ -2,11 +2,11 @@
 
 /* Problem 1 */
 const loaf = {
-    flour: 300,
-    water: 210,
-    hydration: function() {
-        return this.water / this.flour * 100;
-    }
+  flour: 300,
+  water: 210,
+  hydration: function() {
+    return this.water / this.flour * 100;
+  }
 };
 
 //console.log(`loaf has ${loaf.flour} flour and ${loaf.water} water`);
@@ -15,13 +15,20 @@ const loaf = {
 /*Problem 2 */
 
 let item = {
-    foo: 1,
-    bar: 2,
-    fum: 3,
-    quux: 4,
-    spam: 5,
+  foo: 1,
+  bar: 2,
+  fum: 3,
+  quux: 4,
+  spam: 5,
 };
 
 for (let i in item) {
-    console.log(i + ":" + item[i]);
+  console.log(i + ":" + item[i]);
 }
+
+/*Problem 3 */
+
+const obj = {
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+console.log(obj.meals[3]);
