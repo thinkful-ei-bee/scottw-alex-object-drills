b80dcdfabd6849a32151688d0ece9cea4a9a1ffc
Alex Varghese <alex.varghese250@gmail.com>
2019-02-21 12:23:42 -0500

    Prob 2

--- object-drills3.js
+++ object-drills3.js
@@ -1,12 +1,27 @@
 'use strict';
 
+/* Problem 1 */
 const loaf = {
-  flour: 300,
-  water: 210,
-  hydration: function() {
-    return this.water / this.flour * 100;
-  }
-}
+    flour: 300,
+    water: 210,
+    hydration: function() {
+        return this.water / this.flour * 100;
+    }
+};
 
-console.log(`loaf has ${loaf.flour} flour and ${loaf.water} water`);
-console.log(`flour hydration is ${loaf.hydration()}`);
+//console.log(`loaf has ${loaf.flour} flour and ${loaf.water} water`);
+//console.log(`flour hydration is ${loaf.hydration()}`);
+
+/*Problem 2 */
+
+let item = {
+    foo: 1,
+    bar: 2,
+    fum: 3,
+    quux: 4,
+    spam: 5,
+};
+
+for (let i in item) {
+    console.log(i + ":" + item[i]);
+}
